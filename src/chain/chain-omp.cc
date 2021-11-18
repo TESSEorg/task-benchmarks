9c66330b491a286372e9121fa3d89bdfdc1968b3
Joseph Schuchart <schuchart@icl.utk.edu>
2021-11-18 16:43:33 -0500

    Replace chrono code with util/chrono.h
    Signed-off-by: Joseph Schuchart <schuchart@icl.utk.edu>

--- src/chain/chain-omp.cc
+++ src/chain/chain-omp.cc
@@ -1,21 +1,8 @@
 #include <omp.h>
 #include <iostream>
 #include <atomic>
-#include <chrono>
 
-#define USE_CONDITIONAL_TASK
-
-using time_point = std::chrono::high_resolution_clock::time_point;
-
-inline time_point now() { return std::chrono::high_resolution_clock::now(); }
-
-inline std::chrono::system_clock::time_point system_now() {
-  return std::chrono::system_clock::now();
-}
-
-inline int64_t duration_in_mus(time_point const &t0, time_point const &t1) {
-  return std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count();
-}
+#include "../util/chrono.h"
 
 std::atomic<int> task_counter = 0;
 static int N = 10000000;
